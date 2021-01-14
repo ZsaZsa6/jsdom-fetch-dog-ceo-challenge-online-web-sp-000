448bea2e29285adc41cf4b4e2e5ce430d12f09c6
Ronda Cowen <cowenhome5@gmail.com>
2021-01-13 19:44:32 -0600

    commit tree 7f424ed80d91360afb33d62cf094c4db2d7b898c

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all' 
+
+document.addEventListener("DOMContentLoaded", function () {
+    loadImages();
+
+
+});
+
+function loadImages() {
+    fetch(imgUrl)
+    .then(res=> res.json())
+    .then(results => {
+       results.message.forEach(image => newImage(image))
+});
+}
+function newImage() {
+    
+}
